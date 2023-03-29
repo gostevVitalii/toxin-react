ee94d4735377e7ac41800741f926cebf411d2625
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2023-03-29 21:59:03 +0300

    Made header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import { NavLink } from "react-router-dom"
 import Logo from "../Logo/Logo"
-import s from './Header.module.css'
+import Button from "../ui-kit/Button/Button"
+import style from './Header.module.css'
 
 const Header = () => {
   return (
-    <div className={s.header}>
+    <div className={style.header}>
       <Logo />
-      <NavLink to="/signin">Sign-In</NavLink>
-      <NavLink to="/signup">Sign-Up</NavLink>
+      <div className={style.content}>
+        <nav>
+          <NavLink to="/signin">Sign-In</NavLink>
+          <NavLink to="/signup">Sign-Up</NavLink>
+        </nav>
+        <div className={style.buttons}>
+          <Button text={'Log in'} />
+          <Button text={'Sign up'} />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ui-kit/Button/Button.jsx
@@ -0,0 +1,8 @@
+import s from './Button.module.css'
+
+const Button = (props) => {
+  return (
+    <button>{props.text}</button>
+  )
+}
+export default Button
